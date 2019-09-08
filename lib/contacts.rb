6a0dcd604325521932e67129cb9417466eb6a50b
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-09-08 05:33:34 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,132 +1,27 @@
-def hopper
-  programmer_hash = 
-    {
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
+require 'pry'
 
-	programmer_hash[:grace_hopper]
-end
+  # This is the array we will be passing into the remove_strawberry method
+  # contacts = {
+  #   "Jon Snow" => {
+  #     name: "Jon",
+  #     email: "jon_snow@thewall.we",
+  #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+  #   },
+  #   "Freddy Mercury" => {
+  #     name: "Freddy",
+  #     email: "freddy@mercury.com",
+  #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  #   }
+  # }
 
-def alan_kay_is_known_for
-  # What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-  
-  programmer_hash = 
-    {
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-  programmer_hash[:alan_kay][:known_for]
-end
-
-def dennis_ritchies_language
-  programmer_hash = 
-    {
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-
-	programmer_hash[:dennis_ritchie][:languages][0]
-end
-
-def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
-  programmer_hash = 
-    {
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-
-		 programmer_hash[:yukihiro_matsumoto] = {}
-		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-		 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
-		 programmer_hash
-end
-
-def changing_alan
-  programmer_hash = 
-    {
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-     #change what Alan Kay is :known_for the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-
-	programmer_hash[:alan_kay][:known_for] = alans_new_info
-	programmer_hash
-end
-
-def adding_to_dennis
-	programmer_hash =
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-	programmer_hash[:dennis_ritchie][:languages] << "Assembly"
-	programmer_hash
+def remove_strawberry(contacts)
+  contacts.each do |person, contact_details_hash|
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
+      end
+    end
+  end
 end
